0b8a4399fc2196de7a7b8e796ed668756ffe575e
Remco <remco@yosora.com>
2025-05-25 18:13:32 +0200

    clean up of imports

--- app/src/main/java/com/yosora/registrationform/SummaryActivity.kt
+++ app/src/main/java/com/yosora/registrationform/SummaryActivity.kt
@@ -1,10 +1,7 @@
 package com.yosora.registrationform
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.yosora.loginform.Registration
 import com.yosora.registrationform.databinding.ActivitySummaryBinding
 
