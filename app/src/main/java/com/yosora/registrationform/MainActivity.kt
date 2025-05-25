97a18b69fc51e0344027a480286c4f2a986b3131
Remco <remco@yosora.com>
2025-05-25 17:59:19 +0200

    Setup binding

--- app/src/main/java/com/yosora/registrationform/MainActivity.kt
+++ app/src/main/java/com/yosora/registrationform/MainActivity.kt
@@ -5,10 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.yosora.registrationform.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Initialize variable for binding the id's from the layout
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Setup the binding of the id's to the layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
     }
 }
