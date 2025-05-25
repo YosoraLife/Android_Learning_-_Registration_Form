1119bfe870066ba75865306ec194c9f96b6d8887
Remco <remco@yosora.com>
2025-05-25 18:12:51 +0200

    Setup the summaryActivity binding & display of summary

--- app/src/main/java/com/yosora/registrationform/SummaryActivity.kt
+++ app/src/main/java/com/yosora/registrationform/SummaryActivity.kt
@@ -5,11 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.yosora.loginform.Registration
+import com.yosora.registrationform.databinding.ActivitySummaryBinding
 
 class SummaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySummaryBinding
+
+    private lateinit var registration: Registration
+    private lateinit var summaryText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_summary)
+        binding = ActivitySummaryBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+        displaySummary()
+    }
 
+    private fun displaySummary() {
+        // Get the data as a instance of data class Registration
+        registration = intent.getSerializableExtra("Registration") as Registration
+        summaryText = """
+            Registration summary
+            
+            ${registration.title} ${registration.firstName} ${registration.lastName}
+            Email: ${registration.emailAddress}
+            Phone: ${registration.phoneNumber}
+            Password: ${registration.password}
+        """.trimIndent()
+        binding.textViewSummary.text = summaryText
     }
 }
